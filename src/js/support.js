b61bec2132b5d26146ae753a6779cc38c66437eb
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-06-26 18:24:36 +0200

    support

--- src/js/support.js
+++ src/js/support.js
@@ -2,47 +2,47 @@ const organization = [
     {
         title: 'Save the Children',
         url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-        img: null,
+        img: '../images/js-project-png/STC_x1.png',
     },
     {
         title: 'Project HOPE',
         url: 'https://www.projecthope.org/country/ukraine/',
-        img: null,
+        img: '../images/js-project-png/project-hope_x1.png',
     },
     {
         title: 'UNITED24',
         url: 'https://u24.gov.ua/uk',
-        img: null,
+        img: '../images/js-project-png/u24_x1.png',
     },
     {
         title: 'International Medical Corps',
         url: 'https://internationalmedicalcorps.org/country/ukraine/',
-        img: null,
+        img: '../images/js-project-png/IMC_x1.png',
     },
     {
         title: 'Medicins Sans Frontieres',
         url: 'https://www.msf.org/ukraine',
-        img: null,
+        img: '../images/js-project-png/MSF_x1.png',
     },
     {
         title: 'RAZOM',
         url: 'https://www.razomforukraine.org/',
-        img: null,
+        img: '../images/js-project-png/razom_x1.png',
     },
     {
         title: 'Action against hunger',
         url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-        img: null,
+        img: '../images/js-project-png/AAH_x1.png',
     },
     {
         title: 'World vision',
         url: 'https://www.wvi.org/emergencies/ukraine',
-        img: null,
+        img: '../images/js-project-png/WV_x1.png',
     },
     {
         title: 'Serhiy Prytula Charity Foundation',
         url: 'https://prytulafoundation.org/en',
-        img: null,
+        img: '../images/js-project-png/segiy-prytula_x1.png',
     },
 ];
 
@@ -70,9 +70,8 @@ function createSupportList(organization) {
 }
 supportList.insertAdjacentHTML('beforeend', createSupportList(organization));
 
+// function handlerClickMore() {
+//     if (amount < supportList.length {
 
-function handlerClickMore() {
-    if (amount < supportList.length {
-        
-    })
-}
+//     })
+// }

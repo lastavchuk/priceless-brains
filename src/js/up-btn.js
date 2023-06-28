4fa518cf1316ebe36b56592e5e09308e16f8db05
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-06-28 22:48:15 +0200

    support js & to-top-btn

--- src/js/up-btn.js
+++ src/js/up-btn.js
@@ -1,11 +1,17 @@
-(() => {
-    const scrollUp = document.querySelector('.scroll-up');
+const scrollUp = document.querySelector('.scroll-up');
 
-    const showBtn = window.addEventListener('scroll', () => {
-        if (window.scrollY > 600) {
-            scrollUp.classList.add('scroll-up');
-        } else {
-            scrollUp.classList.remove('scroll-up');
-        }
-    });
-})();
+window.addEventListener('scroll', handlerScrollDoc);
+
+scrollUp.classList.add('is-hidden');
+
+function handlerScrollDoc() {
+    if (window.scrollY > 600) {
+        scrollUp.classList.remove('is-hidden');
+    } else {
+        scrollUp.classList.add('is-hidden');
+    }
+
+    scrollUp.onclick = () => {
+        window.scrollTo(0, 0);
+    };
+}

12ac0976fe8f2e3388a2c59a3cbc6cbdab16d916
lastavchuk <it.lastavchuk@gmail.com>
2023-06-28 20:17:45 +0300

    fix

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,7 +1,7 @@
 const btnAuthClose = document.querySelector('[data-close-auth]');
 const authModal = document.querySelector('.auth-backdrop');
 
-document.body.style.overflow = 'hidden';
+// document.body.style.overflow = 'hidden';
 
 btnAuthClose.addEventListener('click', () => {
     authModal.classList.add('is-hidden');

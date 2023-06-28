76cb120741c8674ee7afbeac3ba8a725f4bbc804
lastavchuk <it.lastavchuk@gmail.com>
2023-06-28 19:48:07 +0300

    add auth

---
+++ src/js/auth.js
@@ -0,0 +1,9 @@
+const btnAuthClose = document.querySelector('[data-close-auth]');
+const authModal = document.querySelector('.auth-backdrop');
+
+document.body.style.overflow = 'hidden';
+
+btnAuthClose.addEventListener('click', () => {
+    authModal.classList.add('is-hidden');
+    document.body.style.overflow = '';
+});
--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,5 @@ import '../js/support';
 import '../js/modal';
 import '../js/support';
 import '../js/shopping';
+import '../js/auth';
 import '../js/dark-light-theme';

bad254d51c684b506a72ac408da55eaa28852aaa
Andrii Lutsiak <timechain04@gmail.com>
2023-06-29 18:58:34 +0300

    links

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import '../js/components/refs'
 import '../js/burger-menu';
-import '../js/header'
 import '../js/dark-light-theme';
 import '../js/list-category';
 import '../js/support';
@@ -8,7 +7,5 @@ import '../js/shopping';
 import '../js/modal';
 import '../js/auth';
 import '../js/up-btn';
-import '../js/burger-menu';
-import '../js/header'
 import '../js/footer';
 

75e54d08adf9ddd95bc93c7d904ab410bcd8a45c
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-07-01 01:03:54 +0200

    support completed

--- src/js/support.js
+++ src/js/support.js
@@ -1,13 +1,21 @@
 import stc from '../images/js-project-png/STC_x1.png';
 import stc2 from '../images/js-project-png/STC_x2.png';
 import hoppe from '../images/js-project-png/project-hope_x1.png';
+import hoppe2 from '../images/js-project-png/project-hope_x2.png';
 import united from '../images/js-project-png/u24_x1.png';
+import united2 from '../images/js-project-png/u24_x2.png';
 import international from '../images/js-project-png/IMC_x1.png';
+import international2 from '../images/js-project-png/IMC_x2.png';
 import medicins from '../images/js-project-png/MSF_x1.png';
+import medicins2 from '../images/js-project-png/MSF_x2.png';
 import razom from '../images/js-project-png/razom_x1.png';
+import razom2 from '../images/js-project-png/razom_x2.png';
 import action from '../images/js-project-png/AAH_x1.png';
+import action2 from '../images/js-project-png/AAH_x2.png';
 import worldVision from '../images/js-project-png/WV_x1.png';
+import worldVision2 from '../images/js-project-png/WV_x2.png';
 import serhiyPrytula from '../images/js-project-png/segiy-prytula_x1.png';
+import serhiyPrytula2 from '../images/js-project-png/segiy-prytula_x2.png';
 
 const organization = [
     {
@@ -20,49 +28,49 @@ const organization = [
         title: 'Project HOPE',
         url: 'https://www.projecthope.org/country/ukraine/',
         img: hoppe,
-        img2: stc2,
+        img2: hoppe2,
     },
     {
         title: 'UNITED24',
         url: 'https://u24.gov.ua/uk',
         img: united,
-        img2: stc2,
+        img2: united2,
     },
     {
         title: 'International Medical Corps',
         url: 'https://internationalmedicalcorps.org/country/ukraine/',
         img: international,
-        img2: stc2,
+        img2: international2,
     },
     {
         title: 'Medicins Sans Frontieres',
         url: 'https://www.msf.org/ukraine',
         img: medicins,
-        img2: stc2,
+        img2: medicins2,
     },
     {
         title: 'RAZOM',
         url: 'https://www.razomforukraine.org/',
         img: razom,
-        img2: stc2,
+        img2: razom2,
     },
     {
         title: 'Action against hunger',
         url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
         img: action,
-        img2: stc2,
+        img2: action2,
     },
     {
         title: 'World vision',
         url: 'https://www.wvi.org/emergencies/ukraine',
         img: worldVision,
-        img2: stc2,
+        img2: worldVision2,
     },
     {
         title: 'Serhiy Prytula Charity Foundation',
         url: 'https://prytulafoundation.org/en',
         img: serhiyPrytula,
-        img2: stc2,
+        img2: serhiyPrytula2,
     },
 ];
 
@@ -89,11 +97,9 @@ let maxPages = 0;
 let currentPage = 1;
 let isUp = false;
 
-console.log(supportList.scrollHeight);
-console.log(supportList.clientHeight);
 if (supportList.scrollHeight > supportList.clientHeight) {
     const btnMore = document.querySelector('.support-btn-more');
-    supportList.classList.add('list-more');
+    // supportList.classList.add('list-more');
     btnMore.classList.remove('visually-hidden');
     btnMore.addEventListener('click', handlerClickMore);
 }
@@ -115,48 +121,27 @@ function scrollBtn(e) {
 
     if (currentPage !== maxPages && !isUp) {
         currentPage++;
-        if (currentPage === maxPages) changeNapr(e);
+        if (currentPage === maxPages) changeDirec(e);
         return;
     }
     if (currentPage !== 1 && isUp) {
         currentPage--;
-        if (currentPage === 1) changeNapr(e);
+        if (currentPage === 1) changeDirec(e);
         return;
     }
 }
-function changeNapr(e) {
+
+function changeDirec(e) {
     e.currentTarget.classList.toggle('totop');
     isUp = e.currentTarget.classList.contains('totop');
 }
 
-// let itemsVisible;
+function setItemsVisible() {
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        return 4;
+    } else if (window.matchMedia('(min-width:  768px)').matches) {
+        return 6;
+    }
+}
 
-// function setItemsVisible() {
-//     if (window.innerWidth <= 768) {
-//         itemsVisible = 4;
-//         return;
-//     }
-//     itemsVisible = 6;
-// }
-
-// function scrollBtn() {
-//     const position = supportList.scrollTop;
-//     const maxPosition = supportList.scrollHeight - supportList.clientHeight;
-//     const scrollBottom = maxPosition - position;
-//     if (scrollBottom < maxPosition) {
-//         btnMore.classList.add('totop');
-//     }
-
-//     if (!position) {
-//         btnMore.classList.remove('totop');
-//     }
-// }
-
-// function handlerClickMore() {
-//     if (btnMore.classList.contains('totop')) {
-//         supportList.scrollTop -= supportList.clientHeight;
-//     } else {
-//         supportList.scrollTop += supportList.clientHeight;
-//     }
-//     scrollBtn();
-// }
+setItemsVisible();

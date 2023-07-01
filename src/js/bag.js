2cb93af59dc5ae884043a7877bc76ebaad630227
Stepaniuk-Den <128968908+Stepaniuk-Den@users.noreply.github.com>
2023-07-01 11:33:57 +0300

    create bag counter and update modal

--- src/js/bag.js
+++ src/js/bag.js
@@ -1,10 +1,9 @@
-import { getFromLS, addToLS, removeFromLS } from './modal';
+import { getFromLS } from './modal';
 import { KEY_LS } from './modal';
-import { refs } from './modal';
 
 const bag = document.querySelector('.cart');
-const bagContainer = doc.querySelector('.js-bag-container');
-const bagItem = doc.querySelector('.js-bag-item');
+const bagContainer = document.querySelector('.js-bag-container');
+const bagItem = document.querySelector('.js-bag-item');
 
 window.addEventListener('click', checkNumberinLS);
 
@@ -12,11 +11,28 @@ function checkNumberinLS() {
     const books = getFromLS(KEY_LS) || [];
     const bookNumbers = books.length;
     if (!bookNumbers) {
-        bag.classList.remove('js-bag');
+        hideBagCounter();
         return;
     }
-    bag.classList.add('js-bag');
-    bag.classList.remove('js-bag-container');
-    console.log(bookNumbers);
+    addBagCounter();
+    counterBooks(bookNumbers);
     return;
+}
+
+function hideBagCounter() {
+    bag.classList.remove('js-bag');
+    bagContainer.classList.add('visually-hidden');
+    bagItem.classList.add('visually-hidden');
+}
+
+function addBagCounter() {
+    bag.classList.add('js-bag');
+    bagContainer.classList.remove('visually-hidden');
+    bagItem.classList.remove('visually-hidden');
+}
+
+function counterBooks(bookNumbers) {
+    return bookNumbers < 10
+        ? (bagItem.textContent = bookNumbers)
+        : (bagItem.textContent = '9+');
 }

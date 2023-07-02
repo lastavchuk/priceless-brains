1917be90e102ac62b99893d269b2026726c8a41d
Andrii Lutsiak <timechain04@gmail.com>
2023-07-02 17:23:39 +0300

    burger js

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -2,11 +2,9 @@ const modal = document.getElementById('mobile-menu-modal');
 const openButton = document.getElementById('open-menu-burger');
 const closeButton = document.getElementById('close-mobile-modal-btn');
 const burgerSignUpBtn = document.querySelector('.js-burger-sign-up-btn');
-const authModal = document.querySelector('.auth-backdrop');
 
 openButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
-burgerSignUpBtn.addEventListener('click', openAuthModal);
 
 function openModal() {
   modal.classList.add('open');
@@ -29,13 +27,3 @@ function openAuthModal() {
 window.addEventListener('DOMContentLoaded', function () {
   closeButton.classList.add('hidden');
 });
-
-function onSignUpBtnClick() {
-  const hasUserId = Storage.load('userId');
-  if (!hasUserId) {
-    ref.authModal.classList.remove('is-hidden');
-    document.body.style.overflow = 'hidden';
-  } else {
-    ref.logOutBtn.classList.toggle('is-hidden');
-  }
-}
